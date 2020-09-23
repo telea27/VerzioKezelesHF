2924552170bb44ff9b636b520d52a67ea1e754bd
Petrik Lajos Szg <info@petrik.hu>
2020-09-23 13:01:21 +0200

    bekér 10 számot eltárolja listában kiírja

---
+++ VerzioKezelesHF/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerzioKezelesHF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam;
+            List<int> lista = new List<int>();
+            do
+            {
+                Console.WriteLine("Kérek egy számot");
+                szam = Convert.ToInt32(Console.ReadLine());
+                lista.Add(szam);
+            }
+            while (lista.Count!=10);
+            foreach (var item in lista)
+            {
+                Console.Write(item+" ");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
