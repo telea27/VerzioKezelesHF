a1b4c48ae08fb894573ba95567269c1ad0c0b877
Petrik Lajos Szg <info@petrik.hu>
2020-09-23 13:13:05 +0200

    ő mondja meg hány számot akar

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -10,19 +10,23 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
+            int db;
             int szam;
             int atlag;
             int ossz=0;
             List<int> lista = new List<int>();
+            Console.WriteLine("Hány db számot kér?");
+            db = Convert.ToInt32(Console.ReadLine());
             do
             {
+                
                 Console.WriteLine("Kérek egy számot");
                 szam = Convert.ToInt32(Console.ReadLine());
                 lista.Add(szam);
                 ossz += szam;
                 atlag = ossz / 10;
             }
-            while (lista.Count!=10);
+            while (lista.Count!=db);
             Console.WriteLine("Átlag: "+atlag);
 
             Console.ReadKey();
